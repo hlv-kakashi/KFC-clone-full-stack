8e3df3c4ab7adc035baff38fb06bccb3a40162c3
Arpit Surana <suranaarpit34@gmail.com>
2022-06-14 18:28:43 +0530

    Login page UI Done

---
+++ Frontend/kfc/src/Page/Login.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Header from "../components/Header";
+import "../styles/login.css";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <>
+      <Header />
+      <section className="loginSection">
+        <div id="loginSvgdiv">
+          <div className="loginSvgdiv2">
+            <svg
+              id="loginsvgStrip"
+              viewBox="0 0 42 20"
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="none"
+            >
+              <path class="strip-1" d="M0 0h8v20H0z"></path>
+              <path class="strip-2" d="M16 0h8v20h-8z"></path>
+              <path class="strip-3" d="M32 0h8v20h-8z"></path>
+            </svg>
+          </div>
+        </div>
+        <div className="loginmainbox">
+          <h1 id="loginTitle">LOG IN TO KFC</h1>
+          <div className="loginFormdiv">
+              <form action="" id="loginForm">
+                    <div className="input-data">
+                          <input type="email" name="email" id="email" required/>
+                          {/* <div className="underline"></div> */}
+                          <label>Email</label>
+                    </div>
+                    <div id="loginerrorBox"></div>
+                    <div className="input-data">
+                          <input type="password" name="password" id="password" required/>
+                          {/* <div className="underline"></div> */}
+                          <label>Password</label>
+                    </div>
+                    <div id="loginerrorBox"></div>
+                    <div className="loginterms">
+                        <p className="logintermtext">By logging into the application or proceeding as a guest, you agree to our <span className="logintermlink">Privacy Policy</span> and <span className="logintermlink">Terms of Use</span>.</p>
+                    </div>
+                    <div className="loginBtndiv">
+                      <input type="submit" value="Log In" />
+                    </div>
+                    <div className="redirecttosignup">
+                      <p className="redirectsignuptext">Don't have an account? <Link to={"/Auth/signup"}><span className="redirectsignuplink">Join Now</span></Link></p>
+                    </div>
+              </form>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Login;
---
+++ Frontend/kfc/src/routes/AuthRoute.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "../Page/Login";
+import PageNotFound from "../Page/PageNotFound";
+import Redirect from "../Page/Redirect";
+import SignUp from "../Page/SignUp";
+
+const AuthRoute = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Redirect />}></Route>
+        <Route path="/Auth/login" element={<Login />}></Route>
+        <Route path="/Auth/signup" element={<SignUp />}></Route>
+        {/* page not found */}
+        <Route path="*" element={<PageNotFound />}></Route>
+      </Routes>
+    </>
+  );
+};
+
+export default AuthRoute;
